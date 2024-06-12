94c76b8e181791d05245c384fdc2701521134a71
thejae-u <31125543+thejae-u@users.noreply.github.com>
2024-06-12 14:34:48 +0900

    update: 코드정리

--- GameServer/GameServer/Main.cpp
+++ GameServer/GameServer/Main.cpp
@@ -1,12 +1,14 @@
- #pragma comment(lib, "ws2_32.lib")
+#pragma comment(lib, "ws2_32.lib")
 #include "TCPServer.h"
 
 const int PORT = 56000;
 
- int main() {
+int main()
+{
     TCPServer server(PORT);
     if (server.start()) {
         server.run();
     }
+
     return 0;
 }
