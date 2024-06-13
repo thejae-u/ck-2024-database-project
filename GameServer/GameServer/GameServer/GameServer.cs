454cf128bd44f9d925e32c3296e5d662cb9e71d2
thejae-u <31125543+thejae-u@users.noreply.github.com>
2024-06-13 15:16:58 +0900

    update: NetworkData 클래스 추가, 네트워크 요청 처리 추가, Log 추가

--- GameServer/GameServer/GameServer/GameServer.cs
+++ GameServer/GameServer/GameServer/GameServer.cs
@@ -3,27 +3,89 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
-class GameSerever
+public class GameSerever
 {
-    const int PORT = 56000;
-    const int BUFFER_SIZE = 1024;
+    public const int PORT = 56000;
+    public const int BUFFER_SIZE = 1024;
 
-    static async Task Main(string[] args)
+    private static readonly Queue<NetworkData> _data = new Queue<NetworkData>();
+    private static Task _dequeueTask;
+
+    private static async Task Main(string[] args)
     {
         TcpListener server = new TcpListener(IPAddress.Any, PORT);
         server.Start();
-        Console.WriteLine("Server listening on port " + PORT);
+        string log = $"{DateTime.Now} Server Started on {PORT}";
+        Console.WriteLine(log);
 
+        _ = Task.Run(HandleDequeueNetworkData);
+
+        log = $"{DateTime.Now} Client Accept Started";
+        Console.WriteLine(log);
         while (true)
         {
             TcpClient client = await server.AcceptTcpClientAsync();
-            Console.WriteLine("Client connected.");
+            log = $"{DateTime.Now} Client {client.Client.RemoteEndPoint} Connected.";
+            Console.WriteLine(log);
             HandleClientAsync(client);
         }
     }
 
-    static async void HandleClientAsync(TcpClient client)
+    private static Task HandleDequeueNetworkData()
+    {
+        string log = $"{DateTime.Now} DequeueHandler Started";
+        Console.WriteLine(log);
+
+        while (true)
+        {
+            if( _data.Count == 0 )
+            {
+                continue;
+            }
+
+            NetworkData networkData = _data.Dequeue();
+            ApplyNetworkRequest(networkData);
+        }
+    }
+
+    private static void ApplyNetworkRequest(NetworkData data)
+    {
+        string log = $"{DateTime.Now} {data.client.Client.RemoteEndPoint} : Request Type \"{data.type}\" Request Data \"{data.data}\"";
+        Console.WriteLine(log);
+        switch (data.type)
+        {
+            case ENetworkDataType.Login:
+                break;
+            case ENetworkDataType.Register:
+                break;
+            case ENetworkDataType.Request:
+                break;
+            case ENetworkDataType.Response:
+                break;
+            case ENetworkDataType.Log:
+                break;
+            case ENetworkDataType.Error:
+                break;
+            case ENetworkDataType.Disconnect:
+                break;
+            case ENetworkDataType.None:
+            default:
+                Console.WriteLine($"Invalid Type Request from {data.client.Client.RemoteEndPoint}");
+                try
+                {
+                    data.client.Close();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                break;
+        }
+    }
+
+    private static async void HandleClientAsync(TcpClient client)
     {
         NetworkStream stream = client.GetStream();
         byte[] buffer = new byte[BUFFER_SIZE];
@@ -33,18 +95,27 @@ class GameSerever
             while (true)
             {
                 int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+
                 if (bytesRead == 0)
                 {
-                    Console.WriteLine("Client disconnected.");
+                    Console.WriteLine($"Disconnected {client.Client.RemoteEndPoint}");
                     break;
                 }
 
-                string receivedMessage = Encoding.UTF8.GetString(buffer, 0, bytesRead);
-                Console.WriteLine("Received: " + receivedMessage);
+                string recvData = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                string type = "";
 
-                byte[] responseMessage = Encoding.UTF8.GetBytes(receivedMessage);
-                await stream.WriteAsync(responseMessage, 0, responseMessage.Length);
-                Console.WriteLine("Sent: " + receivedMessage);
+                int i = 0;
+                while (recvData[i] != ',')
+                {
+                    type += recvData[i++];
+                }
+
+                recvData = recvData.Remove(0, i + 1);
+
+                NetworkData networkData = new NetworkData(client, ConvertStringToNetworkDataType(type), recvData);
+
+                _data.Enqueue(networkData);
             }
         }
         catch (Exception ex)
@@ -55,6 +126,32 @@ class GameSerever
         {
             stream.Close();
             client.Close();
+        }
+    }
+
+    private static ENetworkDataType ConvertStringToNetworkDataType(string typeString)
+    {
+        switch (typeString)
+        {
+            case "Login":
+                return ENetworkDataType.Login;
+            case "Register":
+                return ENetworkDataType.Register;
+            case "Request":
+                return ENetworkDataType.Request;
+            case "Response":
+                return ENetworkDataType.Response;
+            case "Log":
+                return ENetworkDataType.Log;
+            case "Error":
+                return ENetworkDataType.Error;
+            case "Disconnect":
+                return ENetworkDataType.Error;
+
+            // Invalid Area
+            case "None":
+            default:
+                return ENetworkDataType.None;
         }
     }
 }
---
+++ GameServer/GameServer/GameServer/NetworkData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Sockets;
+
+public enum ENetworkDataType
+{
+    None,
+    Login,
+    Register,
+    Request,
+    Response,
+    Log,
+    Error,
+    Disconnect
+}
+
+public class NetworkData
+{
+    public TcpClient client;
+    public DateTime time;
+    public ENetworkDataType type;
+    public string data;
+
+    public NetworkData(TcpClient client, ENetworkDataType type, string data)
+    {
+        this.client = client;
+        this.type = type;
+        this.data = data;
+        time = DateTime.Now;
+    }
+}
